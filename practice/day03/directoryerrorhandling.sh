a974961c7deae4b5ff45bcae61ddbbd8d18ea8fb
Azim6917 <sarwadazim786@gmail.com>
2026-03-23 15:21:32 +0000

    directory error handling added

---
+++ practice/day03/directoryerrorhandling.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+<< Errorhandling
+
+🎯 Task
+
+Write a bash script that:
+
+1️⃣ Takes a directory name as an argument
+2️⃣ If no argument is given → show error
+3️⃣ If directory already exists → show error
+4️⃣ If directory does NOT exist:
+
+Create the directory
+Print: Directory created successfully
+
+Errorhandling
+
+if [ $# -eq 0 ]
+then
+	echo "Error: Directory name not entered"
+	exit 1
+
+fi
+
+folder=$1
+
+if [ ! -d "$folder" ]
+then
+	mkdir $folder
+	echo "$folder has been created Successfully."
+	exit 1
+fi
+
+echo "$folder already exists"
