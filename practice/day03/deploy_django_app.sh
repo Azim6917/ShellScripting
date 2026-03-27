b7b252f8e66ca0f27db9b3fd1d6ec3a05762e1b0
Azim6917 <sarwadazim786@gmail.com>
2026-03-27 06:59:38 +0000

    Django_app in progress

---
+++ practice/day03/deploy_django_app.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+<< task
+Deploying django app and handling code for errors
+task
+
+code_clone() {
+	
+	echo "Cloning the django notes app......"
+	git clone https://github.com/LondheShubham153/django-notes-app.git
+}
+
+install_requirements() {
+	
+	echo "Installing Dependencies...."
+	sudo apt-get install docker.io nginx -y
+
+} 
+
+required_restarts () {
+
+	echo" Restarting docker and nginx..... "
+	sudo chown $USER /var/run/docker.sock
+	sudo systemctl enable docker
+	sudo systemctl enable ngnix
+	sudo systemctl restart docker
+
+}
+
+deploy () {
+
+	echo "Building the app...."
+	docker build -t notes-app .
+	echo "Running the app..."
+	docker run -d -p 8000:8000 notes-app:latest
+
+}
+
+echo "******************* DEPLOYMENT STARTED ***********************"
+if ! code_clone; then
+	echo "The code directory already exist."
+	cd django-notes-app
+	 
+fi
+
+if ! install_requirements; then
+	echo "Installation has failed"
+	exit 1
+fi
+
+if ! required_restarts; then
+	echo "System Fault identified"
+	exit 1
+fi
+deploy
+echo "******************* DEPLOYMENT DONE ***********************"
